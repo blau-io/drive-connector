4bb2c076fae1cb162cdbfd44434c5fb5f084e2ac
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-03 14:01:25 -0500

    added documentation to make golint happy

--- googledrive/googledrive.go
+++ googledrive/googledrive.go
@@ -13,10 +13,14 @@ var (
 	config *oauth2.Config
 )
 
+// AuthURL() returns a URL to the Google OAuth2 login page
 func AuthURL() string {
 	return config.AuthCodeURL("google", oauth2.AccessTypeOffline)
 }
 
+// Config() reads the information from the client_secret.json file and
+// parses it into the global config object, so the other functions can
+// access it.
 func Config(filepath string) error {
 	secret, err := ioutil.ReadFile(filepath)
 	if err != nil {
@@ -31,6 +35,8 @@ func Config(filepath string) error {
 	return nil
 }
 
+// Validate() validates an access code against the oauth2.config object. It
+// then returns the real token togehter with an expiry date.
 func Validate(code string) (string, time.Time, error) {
 	token, err := config.Exchange(oauth2.NoContext, code)
 	return token.AccessToken, token.Expiry, err
