dfeac7a293a6b4a593bcb46903b119e42dfd8a1e
Pierre Beaucamp <mail@pierrebeaucamp.com>
2015-11-03 14:02:46 -0500

    minor syntax change for golint

--- googledrive/googledrive.go
+++ googledrive/googledrive.go
@@ -13,12 +13,12 @@ var (
 	config *oauth2.Config
 )
 
-// AuthURL() returns a URL to the Google OAuth2 login page
+// AuthURL returns a URL to the Google OAuth2 login page
 func AuthURL() string {
 	return config.AuthCodeURL("google", oauth2.AccessTypeOffline)
 }
 
-// Config() reads the information from the client_secret.json file and
+// Config reads the information from the client_secret.json file and
 // parses it into the global config object, so the other functions can
 // access it.
 func Config(filepath string) error {
@@ -35,7 +35,7 @@ func Config(filepath string) error {
 	return nil
 }
 
-// Validate() validates an access code against the oauth2.config object. It
+// Validate validates an access code against the oauth2.config object. It
 // then returns the real token togehter with an expiry date.
 func Validate(code string) (string, time.Time, error) {
 	token, err := config.Exchange(oauth2.NoContext, code)
